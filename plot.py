0a8697b158786e5a63ad1c6b1491e9a0220e8a71
ngkeanchewet <ngkeanchewet@gmail.com>
2022-12-23 17:08:10 +0800

    remove cols

--- plot.py
+++ plot.py
@@ -9,6 +9,6 @@ def get_table(df, glevel, sdate, edate):
     adf['change'] = adf[edate] - adf[sdate]
     adf['change_pct'] = adf['change']/adf[sdate]*100
 
-    return adf.style.format("{:,.0f}").format("{:,.2f}%", subset=['change_pct'])\
+    return adf[['change_pct']].style.format("{:,.0f}").format("{:,.2f}%", subset=['change_pct'])\
             .bar(subset=(adf['change_pct']<0, 'change_pct'), color='#FF6347', align="zero")\
             .bar(subset=(adf['change_pct']>0, 'change_pct'), color="#54C571", align="zero")
